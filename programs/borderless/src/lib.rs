c146a7278d54609e6f118c08b20b2be3e566afff
Lokeshwaran B <lokeshwaran100@gmail.com>
2024-12-02 04:54:55 +0530

    Fix ORCA swap issue

--- programs/borderless/src/lib.rs
+++ programs/borderless/src/lib.rs
@@ -2,9 +2,9 @@ use std::{mem::size_of};
 use anchor_lang::prelude::*;
 use anchor_lang::{
     solana_program::{
-        // program::invoke,
+        program::invoke,
         pubkey::Pubkey,
-        // system_instruction::transfer,
+        system_instruction::transfer,
     }
 };
 use anchor_spl::{
@@ -89,48 +89,50 @@ pub mod borderless {
     pub fn transfer_with_swap(
         ctx: Context<TransferWithSwap>,
         amount: u64,
+        sqrt_price_limit: u128
     ) -> Result<()> {
         let state = &mut ctx.accounts.state;
         let platform_token_account = ctx.accounts.platform_token_account.key();
-        require!(
-            state.platform_usdc_account == platform_token_account ||
-            state.platform_usdt_account == platform_token_account,
-            ErrorCode::IncorrectPlatformWallet);
+        // require!(
+        //     state.platform_usdc_account == platform_token_account ||
+        //     state.platform_usdt_account == platform_token_account,
+        //     ErrorCode::IncorrectPlatformWallet);
 
         let platform_fee = (amount * state.platform_fee_per_10000) / 10000;
         let receiver_amount = amount - platform_fee;
 
-        // let current_balance_wsol = ctx.accounts.token_owner_account_a.amount;
-		// if amount > current_balance_wsol {
-		// 	let required_balance_wsol = amount - current_balance_wsol;
-        //     msg!("required_balance_wsol: {0}", required_balance_wsol);
+        let current_balance_wsol = ctx.accounts.token_owner_account_a.amount;
+		if amount > current_balance_wsol {
+			let required_balance_wsol = amount - current_balance_wsol;
+            msg!("required_balance_wsol: {0}", required_balance_wsol);
 			
-		// 	invoke(
-		// 		&transfer(
-		// 			&ctx.accounts.sender.key(),
-		// 			&ctx.accounts.token_owner_account_a.key(),
-		// 			required_balance_wsol,
-		// 		),
-		// 		&[
-		// 			ctx.accounts.sender.to_account_info().clone(),
-		// 			ctx.accounts.token_owner_account_a.to_account_info().clone(),
-		// 			ctx.accounts.system_program.to_account_info().clone(),
-		// 		],
-		// 	)?;
+			invoke(
+				&transfer(
+					&ctx.accounts.sender.key(),
+					&ctx.accounts.token_owner_account_a.key(),
+					required_balance_wsol,
+				),
+				&[
+					ctx.accounts.sender.to_account_info().clone(),
+					ctx.accounts.token_owner_account_a.to_account_info().clone(),
+					ctx.accounts.system_program.to_account_info().clone(),
+				],
+			)?;
 
-        //     token::sync_native(CpiContext::new(
-        //         ctx.accounts.token_program.to_account_info(),
-        //         token::SyncNative {
-        //             account: ctx.accounts.token_owner_account_a.to_account_info(),
-        //         },
-        //     ))?;
-		// }
+            token::sync_native(CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                token::SyncNative {
+                    account: ctx.accounts.token_owner_account_a.to_account_info(),
+                },
+            ))?;
+		}
+        // msg!("tick_current_index: {}", ctx.accounts.whirlpool.tick_current_index);
 
         // Swap the token
         // let sqrt_price_limit = orca_whirlpools_core::invert_sqrt_price(0);
         SwapCpiBuilder::new(&ctx.accounts.whirlpool_program.to_account_info())
             .token_program(&ctx.accounts.token_program.to_account_info())
-            .token_authority(&ctx.accounts.token_authority.to_account_info())
+            .token_authority(&ctx.accounts.sender.to_account_info())
             .whirlpool(&ctx.accounts.whirlpool.to_account_info())
             .token_owner_account_a(&ctx.accounts.token_owner_account_a.to_account_info())
             .token_vault_a(&ctx.accounts.token_vault_a.to_account_info())
@@ -141,37 +143,37 @@ pub mod borderless {
             .tick_array2(&ctx.accounts.tick_array2.to_account_info())
             .oracle(&ctx.accounts.oracle.to_account_info())
             .amount(amount)
-            // .other_amount_threshold(other_amount_threshold)
-            // .sqrt_price_limit(sqrt_price_limit)
+            .other_amount_threshold(0)
+            .sqrt_price_limit(sqrt_price_limit)
             .a_to_b(true)
             .amount_specified_is_input(true)
             .invoke()?;
 
-        // Transfer platform fee
-        token::transfer(
-            CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                token::Transfer {
-                    from: ctx.accounts.token_owner_account_b.to_account_info(),
-                    to: ctx.accounts.platform_token_account.to_account_info(),
-                    authority: ctx.accounts.sender.to_account_info(),
-                }
-            ),
-            platform_fee
-        )?;
+        // // Transfer platform fee
+        // token::transfer(
+        //     CpiContext::new(
+        //         ctx.accounts.token_program.to_account_info(),
+        //         token::Transfer {
+        //             from: ctx.accounts.token_owner_account_b.to_account_info(),
+        //             to: ctx.accounts.platform_token_account.to_account_info(),
+        //             authority: ctx.accounts.sender.to_account_info(),
+        //         }
+        //     ),
+        //     platform_fee
+        // )?;
 
-        // Transfer remaining to receiver
-        token::transfer(
-            CpiContext::new(
-                ctx.accounts.token_program.to_account_info(),
-                token::Transfer {
-                    from: ctx.accounts.token_owner_account_b.to_account_info(),
-                    to: ctx.accounts.receiver_token_account.to_account_info(),
-                    authority: ctx.accounts.sender.to_account_info(),
-                }
-            ),
-            receiver_amount
-        )?;
+        // // Transfer remaining to receiver
+        // token::transfer(
+        //     CpiContext::new(
+        //         ctx.accounts.token_program.to_account_info(),
+        //         token::Transfer {
+        //             from: ctx.accounts.token_owner_account_b.to_account_info(),
+        //             to: ctx.accounts.receiver_token_account.to_account_info(),
+        //             authority: ctx.accounts.sender.to_account_info(),
+        //         }
+        //     ),
+        //     receiver_amount
+        // )?;
 
         Ok(())
     }
@@ -220,6 +222,7 @@ pub struct TransferDirect<'info> {
     )]
     pub state: Account<'info, BorderlessState>,
     
+    #[account(mut)]
     pub admin: Signer<'info>,
     
     pub sender: Signer<'info>,
@@ -263,15 +266,33 @@ pub struct TransferWithSwap<'info> {
         bump = state.bump,
     )]
     pub state: Account<'info, BorderlessState>,
+    
+    #[account(mut)]
+    pub admin: Signer<'info>,
 
     #[account(mut)]
     pub sender: Signer<'info>,
+    
+    /// CHECK:
+    pub receiver: UncheckedAccount<'info>,
 
+    // #[account(
+    //     init_if_needed,
+    //     payer = admin,
+    //     associated_token::mint = token_mint_b,
+    //     associated_token::authority = receiver,
+    // )]
     #[account(mut)]
-    pub receiver_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub receiver_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
 
+    // #[account(
+    //     init_if_needed,
+    //     payer = admin,
+    //     associated_token::mint = token_mint_b,
+    //     associated_token::authority = admin,
+    // )]
     #[account(mut)]
-    pub platform_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub platform_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
 
     pub token_mint_a: Box<InterfaceAccount<'info, Mint>>,
 
@@ -286,8 +307,8 @@ pub struct TransferWithSwap<'info> {
 
     pub token_program: Program<'info, Token>,
 
-    /// CHECK:
-    pub token_authority: Signer<'info>,
+    // /// CHECK:
+    // pub token_authority: Signer<'info>,
 
     /// CHECK: init by whirlpool
     #[account(mut)]
@@ -296,12 +317,13 @@ pub struct TransferWithSwap<'info> {
     /// CHECK:
     // #[account(mut)]
     // pub token_owner_account_a: UncheckedAccount<'info>,
-    #[account(
-        init_if_needed,
-        payer = sender,
-        associated_token::mint = token_mint_a,
-        associated_token::authority = sender,
-    )]
+    // #[account(
+    //     init_if_needed,
+    //     payer = admin,
+    //     associated_token::mint = token_mint_a,
+    //     associated_token::authority = sender,
+    // )]
+    #[account(mut)]
     pub token_owner_account_a: Box<InterfaceAccount<'info, TokenAccount>>,
 
     /// CHECK: init by whirlpool
@@ -311,12 +333,13 @@ pub struct TransferWithSwap<'info> {
     /// CHECK:
     // #[account(mut)]
     // pub token_owner_account_b: UncheckedAccount<'info>,
-    #[account(
-        init_if_needed,
-        payer = sender,
-        associated_token::mint = token_mint_b,
-        associated_token::authority = sender,
-    )]
+    // #[account(
+    //     init_if_needed,
+    //     payer = admin,
+    //     associated_token::mint = token_mint_b,
+    //     associated_token::authority = sender,
+    // )]
+    #[account(mut)]
     pub token_owner_account_b: Box<InterfaceAccount<'info, TokenAccount>>,
 
     /// CHECK: init by whirlpool
@@ -337,37 +360,6 @@ pub struct TransferWithSwap<'info> {
 
     /// CHECK:
     pub oracle: UncheckedAccount<'info>,
-
-    // pub system_program: Program<'info, System>,
-
-    // pub token_program_a: Program<'info, Token2022>,
-
-    // pub token_program_b: Program<'info, token::Token>,
-
-    // /// CHECK:
-    // pub memo_program: UncheckedAccount<'info>,
-
-    // #[account(
-    //     mut,
-    //     seeds = [b"token_config", admin.key().as_ref(), token_mint_a.key().as_ref()],
-    //     bump)]
-    // pub token_config: Account<'info, TokenConfig>,
-
-    // // pub whirlpool_program: Program<'info, Whirlpool>,
-    // /// CHECK:
-    // pub whirlpool_program: UncheckedAccount<'info>,
-
-    // // pub whirlpools_config: Box<Account<'info, WhirlpoolsConfig>>,
-    // /// CHECK:
-    // pub whirlpools_config: UncheckedAccount<'info>,
-
-    // /// CHECK:
-    // #[account(mut)]
-    // pub tick_arraylower: UncheckedAccount<'info>,
-
-    // /// CHECK:
-    // #[account(mut)]
-    // pub tick_arrayupper: UncheckedAccount<'info>,
 }
 
 #[account]
--- tests/borderless.ts
+++ tests/borderless.ts
@@ -2,9 +2,18 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Borderless } from "../target/types/borderless";
 import { LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-import { createAssociatedTokenAccount, createMint, mintTo, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { AccountLayout, createAssociatedTokenAccount, createMint, mintTo, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { sleep } from "@raydium-io/raydium-sdk-v2";
 import { ASSOCIATED_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
+
+import {
+  // ORCA_WHIRLPOOL_PROGRAM_ID, ORCA_WHIRLPOOLS_CONFIG,
+  PDAUtil, PriceMath, SwapUtils,
+  swapQuoteByInputToken, WhirlpoolContext, buildWhirlpoolClient,
+  increaseLiquidityQuoteByInputToken, decreaseLiquidityQuoteByLiquidity,
+  PoolUtil, IGNORE_CACHE, TickUtil,
+} from "@orca-so/whirlpools-sdk";
+import { TransactionBuilder, resolveOrCreateATA, DecimalUtil, Percentage, Wallet, TransactionBuilderOptions } from "@orca-so/common-sdk";
 
 describe("borderless", () => {
   const provider = anchor.AnchorProvider.env();
@@ -14,28 +23,48 @@ describe("borderless", () => {
 
   const SYSTEM_PROGRAM_ID = anchor.web3.SystemProgram.programId;
   const ANCHOR_PROGRAM_ID = program.programId;
-  const TOKEN_MINT = new PublicKey("So11111111111111111111111111111111111111112");
+  // const TOKEN_MINT = new PublicKey("So11111111111111111111111111111111111111112");
   const SOL_MINT = new PublicKey("So11111111111111111111111111111111111111112");
-  const USDC_MINT = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v");
-  let tokenMint = null;
+  const ORCA_WHIRLPOOL_PROGRAM_ID = new anchor.web3.PublicKey("whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc");
+  const ORCA_WHIRLPOOLS_CONFIG = new anchor.web3.PublicKey("FcrweFY1G9HJAHG5inkGB6pKg1HZ6x9UC2WioAfWrGkR");
+  // let tokenMint = null;
+  // const tokenMint = new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"); // Mainnet USDC
+  // let tokenMint = new PublicKey("BRjpCHtyQLNCo8gqRUr8jtdAj5AjPYQaoqbvcZiHok1k"); // Devnet USDC-dev
+  let tokenMint = new PublicKey("BRjpCHtyQLNCo8gqRUr8jtdAj5AjPYQaoqbvcZiHok1k"); // Devnet Orca USDC
 
   const adminKeypair = (provider.wallet as anchor.Wallet).payer;
   const adminPublicKey = adminKeypair.publicKey;
  
-  // const secretKey = Buffer.from(anchor.web3.Keypair.generate().secretKey);
-  // console.log("Secret Key:", secretKey.toString("hex"));
-  // console.log("Secret Key Array:", Object.values(secretKey));
+  const secretKey = Buffer.from(anchor.web3.Keypair.generate().secretKey);
+  console.log("Secret Key:", secretKey.toString("hex"));
+  console.log("Secret Key Array:", Object.values(secretKey));
   // const secretKey = new Uint8Array([
   //   152, 177, 183, 87, 170, 4, 139, 39, 170, 201, 191, 162, 250, 191, 223, 107,
   //   214, 226, 7, 195, 106, 57, 182, 238, 217, 125, 48, 239, 91, 216, 160, 8, 83,
   //   226, 254, 197, 105, 131, 141, 68, 188, 42, 238, 209, 33, 5, 173, 198, 210,
   //   255, 221, 234, 15, 48, 132, 173, 169, 24, 101, 176, 142, 50, 72, 139
   // ]);
-  // const senderKeypair = anchor.web3.Keypair.fromSecretKey(secretKey);
-  const senderKeypair = anchor.web3.Keypair.generate();
+  const senderKeypair = anchor.web3.Keypair.fromSecretKey(new Uint8Array([
+    66,  86, 112, 147, 179,  96, 223,  65, 244, 140, 114,
+    98, 150, 155, 130, 103, 231,  62,  90, 234,  39, 200,
+   122, 171,  73, 222, 204, 182, 238, 159, 197, 143, 253,
+   210, 219,  48, 118,  70, 193, 182, 130,  58, 131, 138,
+   130, 128,  70, 241, 128, 183,  54,  83, 180,  29, 154,
+    59, 134, 200,  46, 157, 191,  46,  81, 133]
+  ));
+  // const senderKeypair = anchor.web3.Keypair.generate();
   const senderPublicKey = senderKeypair.publicKey;
   
-  const receiverKeypair = anchor.web3.Keypair.generate();
+  const receiverKeypair = anchor.web3.Keypair.fromSecretKey(new Uint8Array([
+      246, 217,  55,  21, 129, 226,  19, 185, 223,  29,  76,
+       48, 201, 238, 216, 246, 146, 165, 189,  18, 166,  64,
+      103, 109, 103, 200, 132,  61, 141,   3,  25, 129, 124,
+       97,  60,  82,  29, 163, 254,  83, 211,  92, 242, 136,
+      165,  55, 251,   9,  88, 155, 103,   4, 143, 203,  92,
+      152,  42, 201, 211,  89,  87,  27,  25, 107
+    ]
+  ));
+  // const receiverKeypair = anchor.web3.Keypair.generate();
   const receiverPublicKey = receiverKeypair.publicKey;
   
   const mintKeypair = anchor.web3.Keypair.generate();
@@ -50,39 +79,45 @@ describe("borderless", () => {
 
   console.log("adminPublicKey:", adminPublicKey.toString());
   console.log("senderPublicKey:", senderPublicKey.toString());
+  console.log("receiverPublicKey:", receiverPublicKey.toString());
   console.log("ANCHOR_PROGRAM_ID:", ANCHOR_PROGRAM_ID.toString());
-  console.log("TOKEN_MINT:", TOKEN_MINT.toString());
+  // console.log("TOKEN_MINT:", TOKEN_MINT.toString());
   console.log("SOL_MINT:", SOL_MINT.toString());
   console.log("borderlessStatePdaAccount:", borderlessStatePdaAccount.toString());
 
-  it("Initialize!", async () => {
-    const txHash = await initialize();
-    console.log("initialize", txHash);
-  });
+  // it("Initialize!", async () => {
+  //   const txHash = await initialize();
+  //   console.log("initialize", txHash);
+  // });
 
   // it("Uninitialize!", async () => {
   //   const txHash = await uninitialize();
   //   console.log("uninitialize", txHash);
   // });
 
-  it("transferDirect!", async () => {
-    const txHash = await transferDirect();
-    console.log("transferDirect", txHash);
+  // it("transferDirect!", async () => {
+  //   const txHash = await transferDirect();
+  //   console.log("transferDirect", txHash);
+  // });
+
+  it("transferWithSwap!", async () => {
+    const txHash = await transferWithSwap();
+    console.log("transferWithSwap", txHash);
   });
 
   async function initialize() {
     
-    await requestAirdrop(mintPublicKey);
-    await requestAirdrop(senderPublicKey);
-    await requestAirdrop(receiverPublicKey);
+    // await requestAirdrop(mintPublicKey);
+    // await requestAirdrop(senderPublicKey);
+    // await requestAirdrop(receiverPublicKey);
 
-    tokenMint = await createMint(
-      connection,
-      mintKeypair,
-      mintPublicKey,
-      null,
-      9
-    );
+    // tokenMint = await createMint(
+    //   connection,
+    //   mintKeypair,
+    //   mintPublicKey,
+    //   null,
+    //   9
+    // );
 
     const platformTokenAccount = await createAndGetTokenAccount(
       adminPublicKey,
@@ -160,6 +195,112 @@ describe("borderless", () => {
         systemProgram: SYSTEM_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
         associatedTokenProgram: ASSOCIATED_PROGRAM_ID
+      },
+      signers: [senderKeypair, adminKeypair],
+    });
+    // await displayPda();
+    return txHash;
+  }
+
+  async function transferWithSwap() {
+    console.log("Trace 1")
+    const senderWsolAccount = await createAndGetTokenAccount(
+      senderPublicKey,
+      SOL_MINT,
+      TOKEN_PROGRAM_ID
+    );
+    console.log("Trace 2")
+    const senderTokenAccount = await createAndGetTokenAccount(
+      senderPublicKey,
+      tokenMint,
+      TOKEN_PROGRAM_ID
+    );
+    console.log("Trace 3")
+    const receiverTokenAccount = await createAndGetTokenAccount(
+      receiverPublicKey,
+      tokenMint,
+      TOKEN_PROGRAM_ID
+    );
+    console.log("Trace 4")
+    const platformTokenAccount = await createAndGetTokenAccount(
+      adminPublicKey,
+      tokenMint,
+      TOKEN_PROGRAM_ID
+    );
+    console.log("Trace 5")
+    await sleep(5000);
+    console.log("Trace 6")
+    
+    const SOL = {mint: new PublicKey("So11111111111111111111111111111111111111112"), decimals: 9};
+    // const SOL = {mint: new PublicKey("4ShvTPQ3jYZzwUpxoQFSCDZxLtxQYNPUfeL3sR9mzLjJ"), decimals: 9};
+    // const USDC = {mint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"), decimals: 6};
+    // const USDC = {mint: new PublicKey("9eKgmUSfTkQLRBvowV9zjY3BbhAQVaGSw1jfon5UwUJM"), decimals: 6};
+    const USDC = {mint: new PublicKey("BRjpCHtyQLNCo8gqRUr8jtdAj5AjPYQaoqbvcZiHok1k"), decimals: 6};
+    const sol_usdc_whirlpool_pubkey = PDAUtil.getWhirlpool(ORCA_WHIRLPOOL_PROGRAM_ID, ORCA_WHIRLPOOLS_CONFIG, SOL.mint, USDC.mint, 64).publicKey;
+
+    const whirlpool_ctx = WhirlpoolContext.withProvider(provider, ORCA_WHIRLPOOL_PROGRAM_ID);
+    const fetcher = whirlpool_ctx.fetcher;
+    const sol_usdc_whirlpool_oracle_pubkey = PDAUtil.getOracle(ORCA_WHIRLPOOL_PROGRAM_ID, sol_usdc_whirlpool_pubkey).publicKey;
+    const sol_usdc_whirlpool = await fetcher.getPool(sol_usdc_whirlpool_pubkey);
+
+    const sol_input = DecimalUtil.toBN(DecimalUtil.fromNumber(0.1 /* SOL */), SOL.decimals);
+    console.log("sol_input: ", sol_input.toString())
+    const wallet = senderKeypair;
+    const rent_ta = async () => { return connection.getMinimumBalanceForRentExemption(AccountLayout.span) }
+    const wsol_ta = await resolveOrCreateATA(connection, wallet.publicKey, SOL.mint, rent_ta, sol_input);
+    const usdc_ta = await resolveOrCreateATA(connection, wallet.publicKey, USDC.mint, rent_ta);
+
+    const amount = new anchor.BN(sol_input);
+    const other_amount_threshold = new anchor.BN(0);
+    const amount_specified_is_input = true;
+    const a_to_b = true;
+    const sqrt_price_limit = SwapUtils.getDefaultSqrtPriceLimit(a_to_b);
+    
+    const tickarrays = SwapUtils.getTickArrayPublicKeys(
+      sol_usdc_whirlpool.tickCurrentIndex,
+      sol_usdc_whirlpool.tickSpacing,
+      a_to_b,
+      ORCA_WHIRLPOOL_PROGRAM_ID,
+      sol_usdc_whirlpool_pubkey
+    );
+    console.log("Trace 7")
+
+    console.log("tickarrays[0]", tickarrays[0].toString());
+    console.log("tickarrays[1]", tickarrays[1].toString());
+    console.log("tickarrays[2]", tickarrays[2].toString());
+    console.log("wsol_ta.address", wsol_ta.address.toString());
+    console.log("senderWsolAccount", senderWsolAccount.toString());
+    console.log("usdc_ta.address", usdc_ta.address.toString());
+    console.log("senderTokenAccount", senderTokenAccount.toString());
+
+    let txHash = await program.rpc.transferWithSwap(
+      new anchor.BN(0.1 * LAMPORTS_PER_SOL),
+      sqrt_price_limit,
+      {accounts: {
+        state: borderlessStatePdaAccount,
+        admin: adminPublicKey,
+        sender: senderPublicKey,
+        receiver: receiverPublicKey,
+        receiverTokenAccount: receiverTokenAccount,
+        platformTokenAccount: platformTokenAccount,
+        tokenMintA: SOL.mint,
+        tokenMintB: USDC.mint,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_PROGRAM_ID,
+        whirlpoolProgram: ORCA_WHIRLPOOL_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        // tokenAuthority: senderPublicKey,
+        whirlpool: sol_usdc_whirlpool_pubkey,
+        tokenOwnerAccountA: senderWsolAccount,
+        // tokenOwnerAccountA: wsol_ta.address,
+        tokenVaultA: sol_usdc_whirlpool.tokenVaultA,
+        tokenOwnerAccountB: senderTokenAccount,
+        // tokenOwnerAccountB: usdc_ta.address,
+        tokenVaultB: sol_usdc_whirlpool.tokenVaultB,
+        tickArray0: tickarrays[0],
+        tickArray1: tickarrays[1],
+        tickArray2: tickarrays[2],
+        oracle:sol_usdc_whirlpool_oracle_pubkey
       },
       signers: [senderKeypair, adminKeypair],
     });
